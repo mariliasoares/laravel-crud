a0ca0b0e020c572a9fb790d059724335619117e8
Marilia Soares <marisoares1202@gmail.com>
2019-05-30 02:31:55 -0400

    text to number

--- resources/views/boats/_partials/boats-form.blade.php
+++ resources/views/boats/_partials/boats-form.blade.php
@@ -9,7 +9,7 @@
     <input type="text" value="{{$boat->cidade ?? old('cidade')}}" class="form-control" name="cidade" id="cidade">
 
     <label for="boatSize" class="">Tamanho do barco</label>
-    <input type="text" value="{{$boat->boatSize ?? old('boatSize')}}" class="form-control" name="boatSize" id="boatSize">
+    <input type="number" value="{{$boat->boatSize ?? old('boatSize')}}" class="form-control" name="boatSize" id="boatSize">
 
     <label for="boatDescription" class="">Descriçao</label>
     <input type="text" value="{{$boat->boatDescription ?? old('boatDescription')}}" class="form-control" name="boatDescription" id="boatDescription">
